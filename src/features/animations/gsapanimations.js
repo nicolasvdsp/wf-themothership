7563e5c5f96cdf821b4efcdc49ca1e641daa929c
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2025-03-21 17:54:50 +0100

    feat(h1): joystick icon - makes stick follow mouse position

--- src/features/animations/gsapanimations.js
+++ src/features/animations/gsapanimations.js
@@ -92,9 +92,35 @@ function alianAnimation() {
   })
 }
 
+function dragJoystick() {
+
+  const joystick = document.querySelector('[data-icon-type="stick"]');
+  if (joystick) {
+    const maxRotation = 15; // Max 15 graden naar links/rechts
+    const centerX = window.innerWidth / 2; // Center van het scherm (horizontaal)
+
+    // Zet de rotatie oorsprong (center-bottom)
+    joystick.style.transformOrigin = "50% 60%";
+
+    document.addEventListener("mousemove", (event) => {
+      const mouseX = event.clientX; // Muispositie op de X-as
+
+      // Bereken het verschil tussen de muispositie en het midden van het scherm
+      const deltaX = mouseX - centerX;
+
+      // Beperk de rotatie tot max 15 graden
+      const rotation = Math.max(-maxRotation, Math.min(maxRotation, deltaX / 10)); // deltaX wordt gedeeld om de rotatie geleidelijker te maken
+
+      // Gebruik GSAP om de rotatie van de joystick te veranderen
+      gsap.to(joystick, { rotation: rotation });
+    });
+  }
+}
+
 function allGsapAnimations() {
   initScrambleOnHover()
   alianAnimation()
+  dragJoystick()
 }
 
 export default allGsapAnimations;
